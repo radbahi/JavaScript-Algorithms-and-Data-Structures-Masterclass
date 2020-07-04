a30d214f185dca8da2cb850bde9f64a8f91eb606
Radouane Bahi <radbahi@gmail.com>
2020-07-04 11:26:37 -0400

    count unique values comparison

---
+++ Section 5: Problem Solving Patterns/count-unique-values-solution.js
@@ -0,0 +1,28 @@
+// https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/11183948#notes
+
+function countUniqueValues(arr) { // my solution that works
+    // add whatever parameters you deem necessary - good luck!
+    if (arr.length < 1) {
+        return 0
+    }
+    let uniqueCounter = 1
+    let currentValue = arr[0]
+    for (let index of arr) {
+        if (index !== currentValue) {
+          uniqueCounter++
+          currentValue = index
+        }
+    }
+    return uniqueCounter
+  }
+
+function countUniqueValues(arr) { // instructor's solution
+  let i = 0
+  for (let j = 1; j < arr.length; j++) {
+      if (arr[i] !== arr[j]) {
+          i++
+          arr[i] = arr[j]
+      }
+  }
+  return i + 1
+}
--- Section 5: Problem Solving Patterns/multiple-pointers-pattern.js
+++ Section 5: Problem Solving Patterns/multiple-pointers-pattern.js
@@ -18,7 +18,7 @@ function sumZero(arr) {
     while (left < right) {
         let sum = arr[left] + arr[right]
         if (sum === 0) {
-            return [arr[left, arr[right]]]
+            return [arr[left], arr[right]]
         } else if (sum > 0) {
             right--
         } else {
