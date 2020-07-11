9d93a8bf67ba49a9fecc1bfe6317ce878e676956
Radouane Bahi <radbahi@gmail.com>
2020-07-11 13:24:42 -0400

    pure recursion

--- Section 7: Recursion/helper-method-recursion.js
+++ Section 7: Recursion/helper-method-recursion.js
@@ -1,7 +1,7 @@
 // https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/10046464#notes
 
 function collectOddValues(arr) {
-    let result = []
+    let result = [] // this is defined outside to prevent it resetting to empty
     function helper(helperInput) { // this is just to show off a recursive helper method. obviously not the only way to do this.
         if (helperInput.length === 0) {
             return
