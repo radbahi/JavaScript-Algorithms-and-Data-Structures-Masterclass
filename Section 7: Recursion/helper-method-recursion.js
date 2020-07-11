a42d2da54246325f0f6955eb6ddd53120c511b4f
Radouane Bahi <radbahi@gmail.com>
2020-07-11 12:54:24 -0400

    recursive helper methods

---
+++ Section 7: Recursion/helper-method-recursion.js
@@ -0,0 +1,18 @@
+// https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/10046464#notes
+
+function collectOddValues(arr) {
+    let result = []
+    function helper(helperInput) { // this is just to show off a recursive helper method. obviously not the only way to do this.
+        if (helperInput.length === 0) {
+            return
+        }
+
+        if (helperInput[0] % 2 !== 0) {
+            result.push(helperInput[0])
+        }
+
+        helper(helperInput.slice(1))
+    }
+    helper(arr)
+    return result
+}
